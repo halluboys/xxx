4c198a7f45f7bfa6d603321fb5d2b8bc5ede2ed3
halluboys <87511503+halluboys@users.noreply.github.com>
2022-03-03 23:02:51 +0700

    Update menu-trojan.sh

--- menu-trojan.sh
+++ menu-trojan.sh
@@ -19,19 +19,7 @@ red='\e[1;31m'
 green='\e[1;32m'
 NC='\e[0m'
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-echo -ne
-else
-red "Permission Denied!"
-exit 0
-fi
-clear
+red() { echo -e "\\033[31;1m${*}\\033[0m"; } 
 
 x="ok"
 cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
