5ebe48f2f7212ebe3c848e1426581bd64b863187
halluboys <87511503+halluboys@users.noreply.github.com>
2022-03-06 10:35:57 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -19,14 +19,6 @@ white='\e[1;37m'
 NC='\e[0m'
 MYIP=$(curl -sS ipv4.icanhazip.com)
 #########################
-IZIN=$(curl -sS https://raw.githubusercontent.com/halluboys/perizinan/main/main/allow | awk '{print $4}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mDaftar IP dalam github lok sayang okay? mun dah daftar tapi masih juak permission denied refresh dolok website ya hehe. Love you #\e[0m"
-exit 0
-fi
 #EXPIRED
 expired=$(curl -sS https://raw.githubusercontent.com/halluboys/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
 echo $expired > /root/expired.txt
