33ceb3e338debbc001ff5d2bfbeab93cadc7c754
halluboys <87511503+halluboys@users.noreply.github.com>
2022-03-06 07:43:29 +0700

    Create 2.sh

---
+++ update/2.sh
@@ -0,0 +1,25 @@
+cd /usr/bin
+
+wget -O triall2tp "https://raw.githubusercontent.com/halluboys/xxx/main/trial/triall2tp.sh"
+wget -O trialpptp "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialpptp.sh"
+wget -O trialss "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialss.sh"
+wget -O trialssh "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialssh.sh"
+wget -O trialssr "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialssr.sh"
+wget -O trialsstp "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialsstp.sh"
+wget -O trialtrgo "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialtrgo.sh"
+wget -O trialtrojan "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialtrojan.sh"
+wget -O trialv2ray "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialv2ray.sh"
+wget -O trialvless "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialvless.sh"
+wget -O trialwg "https://raw.githubusercontent.com/halluboys/xxx/main/trial/trialwg.sh"
+
+chmod +x triall2tp
+chmod +x trialpptp
+chmod +x trialss
+chmod +x trialssh
+chmod +x trialssr
+chmod +x trialtrgo
+chmod +x trialsstp
+chmod +x trialtrojan
+chmod +x trialv2ray
+chmod +x trialvless
+chmod +x trialwg
