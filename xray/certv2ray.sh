9d6818abf23d104fc75d4faecee85e2b4aff7903
halluboys <87511503+halluboys@users.noreply.github.com>
2022-03-08 23:20:30 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -28,3 +28,5 @@ echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
 bash acme.sh --register-account -m hammysayang@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
 bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+
+restart
