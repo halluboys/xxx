b8d7d5a18228dde1c5321434192e9456bb56e4e0
halluboys <87511503+halluboys@users.noreply.github.com>
2022-09-10 19:43:10 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -18,7 +18,7 @@ rm -rf /root/.acme.sh
 cp /root/domain /etc/xray/domain
 source /var/lib/akbarstorevpn/ipvps.conf
 domain=$(cat /etc/xray/domain)
-sudo lsof -t -i tcp:89 -s tcp:listen | sudo xargs kill
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 cd /root/
 wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
 bash acme.sh --install
