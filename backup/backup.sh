630e955cfedc2825c566b7491ab45bfb7374af53
halluboys <87511503+halluboys@users.noreply.github.com>
2024-05-26 22:20:27 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# My Telegram : https://t.me/Akbar218
+# My Telegram : https://t.me/haluboy
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -32,15 +32,15 @@ cp /etc/passwd backup/
 cp /etc/group backup/
 cp /etc/shadow backup/
 cp /etc/gshadow backup/
-cp -r /etc/wireguard backup/wireguard
+#cp -r /etc/wireguard backup/wireguard
 cp /etc/ppp/chap-secrets backup/chap-secrets
 cp /etc/ipsec.d/passwd backup/passwd1
-cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
-cp -r /var/lib/akbarstorevpn/ backup/akbarstorevpn
-cp -r /home/sstp backup/sstp
+#cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
+#cp -r /var/lib/akbarstorevpn/ backup/akbarstorevpn
+#cp -r /home/sstp backup/sstp
 cp -r /etc/xray backup/xray
-cp -r /etc/trojan-go backup/trojan-go
-cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
+#cp -r /etc/trojan-go backup/trojan-go
+#cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
 cp -r /home/vps/public_html backup/public_html
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
